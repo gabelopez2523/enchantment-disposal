6f3228814338afecbbf7a93ab38789078d5319c6
Gabe Lopez <gabe.lopez2523@gmail.com>
2025-10-19 22:17:38 -0600

    Enable Vercel Web Analytics II

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Enchantment Disposal",
@@ -27,6 +28,7 @@ export default function RootLayout({
         <main className="flex-grow">{children}</main>
 
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
